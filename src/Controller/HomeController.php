79898906faaf3ec5ba5b62d9c2d99f0868120024
aurelien <aureliendrey@gmail.com>
2020-08-22 12:02:23 +0200

    First Commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/test")
+     */
+    public function test()
+    {
+        return new Response("TEST");
+    }
+}
