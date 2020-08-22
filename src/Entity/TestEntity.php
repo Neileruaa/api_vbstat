fba05370cd0ab2ec99b0996b366b7cc7493cd77d
aurelien <aureliendrey@gmail.com>
2020-08-22 13:01:25 +0200

    Doctrine

---
+++ src/Entity/TestEntity.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TestEntityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TestEntityRepository::class)
+ */
+class TestEntity
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $testField;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTestField(): ?string
+    {
+        return $this->testField;
+    }
+
+    public function setTestField(string $testField): self
+    {
+        $this->testField = $testField;
+
+        return $this;
+    }
+}
